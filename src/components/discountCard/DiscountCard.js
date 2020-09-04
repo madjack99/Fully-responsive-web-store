3c5828db136241cdc437722e8c5a37510faf7c86
Alex <kavanokami@gmail.com>
2020-09-04 13:18:21 +0400

    feat: add markup for discount card

--- src/components/discountCard/DiscountCard.js
+++ src/components/discountCard/DiscountCard.js
@@ -5,8 +5,14 @@ import './style.css';
 const DiscountCard = () => {
   return (
     <>
-      <div className='DiscountCard'>card</div>
-      <div>card</div>
+      <div className='DiscountCard'>
+        <div className='DiscountCard-before' />
+        <h2 className='DiscountCard-title'>Title</h2>
+        <p className='DiscountCard-message'>message</p>
+        <p className='DiscountCart-amount'>Amount</p>
+        <p className='DiscountCart-description'>Description</p>
+        <div className='DiscountCard-after' />
+      </div>
     </>
   );
 };

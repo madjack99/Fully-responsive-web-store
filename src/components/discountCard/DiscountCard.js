bc045b96aae16fa445637a977ff2cb35edda359c
Alex <kavanokami@gmail.com>
2020-09-05 16:01:00 +0400

    feat: add discount card markup and styles

--- src/components/discountCard/DiscountCard.js
+++ src/components/discountCard/DiscountCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
+import { discountCardsInfo } from '../../settings';
+
 import './style.css';
 
 const DiscountCard = () => {
-  return (
-    <>
-      <div className='DiscountCard'>
-        <div className='DiscountCard-before' />
-        <h2 className='DiscountCard-title'>Title</h2>
-        <p className='DiscountCard-message'>message</p>
-        <p className='DiscountCart-amount'>Amount</p>
-        <p className='DiscountCart-description'>Description</p>
-        <div className='DiscountCard-after' />
-      </div>
-    </>
-  );
+  const renderDiscountCards = () =>
+    discountCardsInfo.map((card, index) => {
+      return (
+        <div className='DiscountCard' key={index}>
+          <div className='DiscountCard-before' />
+          <h3 className='DiscountCard-title'>{card.title}</h3>
+          <p className='DiscountCard-message'>{card.message}</p>
+          <p className='DiscountCart-amount'>{card.amount}</p>
+          <p className='DiscountCart-description'>{card.description}</p>
+          <div className='DiscountCard-after' />
+        </div>
+      );
+    });
+
+  return <>{renderDiscountCards()}</>;
 };
 
 export default DiscountCard;
--- src/settings/index.js
+++ src/settings/index.js
@@ -5,3 +5,27 @@ export const shopDepartmentTitles = [
   'Развлечения',
   'Авто',
 ];
+
+export const discountCardsInfo = [
+  {
+    title: 'пробка на мира',
+    message: 'Вечер для двоих',
+    amount: '50%',
+    description: 'скидка',
+    image: 'wine',
+  },
+  {
+    title: 'кино и немцы',
+    message: 'Бесплатный бокал пива',
+    amount: 'ВАРВАР',
+    description: 'промокод',
+    image: 'wine',
+  },
+  {
+    title: 'valery cakes',
+    message: 'Торт',
+    amount: '30%',
+    description: 'скидка',
+    image: 'wine',
+  },
+];

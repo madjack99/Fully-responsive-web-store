d93bdc51be6bfcb72ddf3a5ed2fbafa6d98322f0
Alex <kavanokami@gmail.com>
2020-09-05 16:37:50 +0400

    feat: add active classes for cards

--- src/components/discountCard/DiscountCard.js
+++ src/components/discountCard/DiscountCard.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 import { discountCardsInfo } from '../../settings';
 
 import './style.css';
 
 const DiscountCard = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
   const renderDiscountCards = () =>
     discountCardsInfo.map((card, index) => {
       return (
-        <div className='DiscountCard' key={index}>
-          <div className='DiscountCard-before' />
+        <div
+          className={`DiscountCard ${
+            index === selectedCard ? 'DiscountCard_active' : ''
+          }`}
+          key={index}
+          onClick={() => setSelectedCard(index)}
+        >
+          <div
+            className={classNames('DiscountCard-before', {
+              'DiscountCard-before_active': index === selectedCard,
+            })}
+          />
           <h3 className='DiscountCard-title'>{card.title}</h3>
           <p className='DiscountCard-message'>{card.message}</p>
           <p className='DiscountCart-amount'>{card.amount}</p>
           <p className='DiscountCart-description'>{card.description}</p>
-          <div className='DiscountCard-after' />
+          <div
+            className={classNames('DiscountCard-after', {
+              'DiscountCard-after_active': index === selectedCard,
+            })}
+          />
         </div>
       );
     });

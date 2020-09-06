ecb9402a3486edf4ebb0240cad129b9b48256c3a
Alex <kavanokami@gmail.com>
2020-09-06 15:36:30 +0400

    feat: add scroll event for input display

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 import { useShopDepartmentContext } from '../../../contexts/shopDepartment';
 
@@ -6,16 +7,39 @@ import './style.css';
 import search from './search.svg';
 
 const Header = () => {
+  const [isHeaderScrolled, setIsHeaderScrolled] = useState(false);
+  const [yOffset, setYoffset] = useState(0);
   const [shopDepartment] = useShopDepartmentContext();
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // console.log(yOffset);
+
+  const handleScroll = () => {
+    setYoffset(window.pageYOffset);
+  };
+
   return (
-    <header className='Header'>
+    <header className={classNames('Header', { Header_scrolled: yOffset > 62 })}>
       <h2 className='Header-title'>{shopDepartment}</h2>
       <form className='ShopSearch'>
-        <img src={search} className='ShopSearch-image' alt='search shop' />
+        <img
+          src={search}
+          className={classNames('ShopSearch-image', {
+            'ShopSearch-image_hidden': yOffset > 62,
+          })}
+          alt='search shop'
+        />
         <input
           type='text'
-          className='ShopSearch-input'
+          className={classNames('ShopSearch-input', {
+            'ShopSearch-input_scrolled': yOffset > 62,
+          })}
           placeholder='Какой магазин вам нужен?'
         />
       </form>

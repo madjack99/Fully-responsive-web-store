b9f0beace9fdbc1585a4edb37a4fee1f710f93a5
Alex <kavanokami@gmail.com>
2020-09-06 16:47:48 +0400

    refactor: create var to trace isScrolled value

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -7,9 +7,11 @@ import './style.css';
 import search from './search.svg';
 
 const Header = () => {
-  const [isHeaderScrolled, setIsHeaderScrolled] = useState(false);
   const [yOffset, setYoffset] = useState(0);
+
   const [shopDepartment] = useShopDepartmentContext();
+
+  const isScrolled = yOffset > 62;
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -18,27 +20,25 @@ const Header = () => {
     };
   }, []);
 
-  // console.log(yOffset);
-
   const handleScroll = () => {
     setYoffset(window.pageYOffset);
   };
 
   return (
-    <header className={classNames('Header', { Header_scrolled: yOffset > 62 })}>
+    <header className={classNames('Header', { Header_scrolled: isScrolled })}>
       <h2 className='Header-title'>{shopDepartment}</h2>
       <form className='ShopSearch'>
         <img
           src={search}
           className={classNames('ShopSearch-image', {
-            'ShopSearch-image_hidden': yOffset > 62,
+            'ShopSearch-image_hidden': isScrolled,
           })}
           alt='search shop'
         />
         <input
           type='text'
           className={classNames('ShopSearch-input', {
-            'ShopSearch-input_scrolled': yOffset > 62,
+            'ShopSearch-input_scrolled': isScrolled,
           })}
           placeholder='Какой магазин вам нужен?'
         />

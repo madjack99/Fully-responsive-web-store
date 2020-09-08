ff621870fde40a794440972167acb6e1f48e8373
Alex <kavanokami@gmail.com>
2020-09-08 17:43:00 +0400

    feat: add responsive logo for desktop

--- src/components/layout/sideNav/SideNav.js
+++ src/components/layout/sideNav/SideNav.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
 import SideNavMenu from './SideNavMenu';
 
 import burgerMenu from './icons/burger-menu.svg';
 import menuExit from './icons/menu-exit.svg';
-import logo from './icons/logo.svg';
+import logoSm from './icons/logo-sm.svg';
+import logoLg from './icons/logo-lg.svg';
 
 import './style.css';
 
 const SideNav = () => {
   const [isNavOpen, setIsNavOpen] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const responsiveLogo = windowWidth > 1440 ? logoLg : logoSm;
   const NavOpenOrCloseIcon = isNavOpen ? menuExit : burgerMenu;
+
+  useEffect(() => {
+    const handleWidthResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWidthResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWidthResize);
+    };
+  }, []);
 
   return (
     <div className={`SideNav ${isNavOpen ? '' : 'SideNav_closed'}`}>
@@ -28,7 +44,7 @@ const SideNav = () => {
           LogoContent_closed: !isNavOpen,
         })}
       >
-        <img src={logo} alt='logo' />
+        <img src={responsiveLogo} alt='logo' />
         <h1
           className={classNames('LogoContent-title', {
             'LogoContent-title_closed': !isNavOpen,
